4c2818fd7b0137f375fb4fdcce00b753e0969e49
Favour Idowu <favouridowu02@gmail.com>
2025-10-05 07:03:18 +0100

    feat: implement the Card component

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,14 @@
+import { CardProps } from "@/interfaces/index";
+import React from "react";
+
+
+const Card: React.FC<CardProps> = ({ title, content}) => {
+    return (
+        <div>
+            <h2>{title}</h2>
+            <p>{content}</p>
+        </div>
+    )
+}
+
+export default Card;
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '@/components/layout/Header';
+import Card from '@/components/common/Card';
 
 const Home: React.FC = () => {
     return (
@@ -7,6 +8,9 @@ const Home: React.FC = () => {
             <Header />
             <h1>Welcome!</h1>
             <p>This is the home page of our application.</p>
+
+            <Card title="This is just the beginning" content="We are going to church and I am coding" />
+            <Card title="I am going to be very great and wealthy" content="God please forgive me of all my sins, I am a sinner" />
         </div>
 
     )
